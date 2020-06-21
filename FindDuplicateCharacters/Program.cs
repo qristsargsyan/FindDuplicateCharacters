1894d0e358c4a76da0df8bf3c1b2aba81f59c560
QRISTINE Sargsyan <qsargsyan96@mail.ru>
2020-06-21 23:59:31 +0400

    Add project files.

---
+++ FindDuplicateCharacters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindDuplicateCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindDoublicateCharactersCount.Find();
+        }
+    }
+}
+    static class FindDoublicateCharactersCount
+    {
+        public static void Find()
+        {
+            string line = Console.ReadLine();
+            char[] alonechar = line.ToCharArray();
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            foreach (var ch in alonechar)
+            {
+                if (dictionary.ContainsKey(ch))
+                {
+                    dictionary[ch] = dictionary[ch] + 1;
+                }
+                else
+                {
+                    dictionary.Add(ch, 1);
+                }
+            }
+            var keys = new HashSet<char>(dictionary.Keys);
+            foreach (var ch in keys)
+            {
+                if (dictionary[ch] >= 1)
+                {
+                    Console.WriteLine($"Character {ch} repeating {dictionary[ch]} times");
+                }
+            }
+        }
+    }
+  
+
